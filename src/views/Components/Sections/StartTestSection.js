8bb759581dd19a08333e2097d14c8d3dcc224093
Tamara Slosarek <tamaraslosarek@gmail.com>
2020-03-21 12:23:03 +0000

    Adapt start page

--- src/views/Components/Sections/StartTestSection.js
+++ src/views/Components/Sections/StartTestSection.js
@@ -11,6 +11,7 @@ import Help from "@material-ui/icons/Help";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import InfoArea from "components/InfoArea/InfoArea.js";
+import Button from "components/CustomButtons/Button.js";
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
 
@@ -24,50 +25,56 @@ export default function ProductSection() {
         <GridItem xs={12} sm={12} md={8}>
           <h2 className={classes.title}>Wie es funktioniert</h2>
           <h5 className={classes.description}>
-            So wirst du durch den Test geleitet:
+            Wir leiten dich durch mehrere Schritte; am Ende hast du alle
+            Informationen, die du brauchst, um entspannt durch den Testprozess
+            zu gehen.
           </h5>
         </GridItem>
       </GridContainer>
       <div>
         <GridContainer>
-          <GridItem xs={12} sm={12} md={3}>
+          <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title="Beantworte Fragen zu deiner Risikoeinschätzung"
-              description="TODO."
+              title="Beantworte einen Fragebogen"
+              description="Mit Deinen Antworten hilfst du dabei, das Krankheitsrisiko einzuschätzen."
               icon={Chat}
               iconColor="info"
               vertical
             />
           </GridItem>
-          <GridItem xs={12} sm={12} md={3}>
+          <GridItem xs={12} sm={12} md={4}>
             <InfoArea
               title="Vervollständige deine Informationen"
-              description="TODO."
+              description="Um den Test möglichst schnell durchführen zu können, werden weitere Daten benötigt."
               icon={VerifiedUser}
               iconColor="success"
               vertical
             />
           </GridItem>
-          <GridItem xs={12} sm={12} md={3}>
+          <GridItem xs={12} sm={12} md={4}>
             <InfoArea
               title="Setze dich mit Test-Centern in deiner Nähe in Verbindung"
-              description="TODO."
+              description="Melde ."
               icon={LocationOn}
               iconColor="danger"
               vertical
             />
           </GridItem>
-          <GridItem xs={12} sm={12} md={3}>
+          <GridItem xs={12} sm={12} md={12}>
             <InfoArea
-              title="Du hast noch Fragen? Hier findest du Informationen"
-              description="TODO."
+              title="Noch etas unklar?"
+              description="Wir helfen Dir die Informationen zu erhalten, die du brauchst."
               icon={Help}
               iconColor="info"
               vertical
             />
           </GridItem>
+          <GridItem xs={12} sm={12} md={12} style={{ marginTop: "24px" }}>
+            <Button type="button" color="success">
+              Fragebogen starten
+            </Button>
+          </GridItem>
         </GridContainer>
-        TODO: Button "Start Test"
       </div>
     </div>
   );
