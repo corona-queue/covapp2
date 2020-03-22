ce78c9523ff5194042fcf999b2422f0ee6f46e41
Michael Janke <micha@jankeweb.de>
2020-03-22 21:16:53 +0100

    warning design

--- src/Warning.js
+++ src/Warning.js
@@ -7,21 +7,23 @@ export default function Warning() {
       style={{
         paddingLeft: "16px",
         paddingRight: "16px",
-        color: "#e91e63",
-        backgroundColor: "rgba(255,255,255,0.9)",
+        color: "white",
+        backgroundColor: "#e91e63",
         display: "flex",
         position: "fixed",
         bottom: 0,
         left: 0,
         right: 0,
         alignItems: "center",
-        justifyContent: "space-around"
+        justifyContent: "center",
+        textAlign: "center",
+        zIndex: 1200
       }}
     >
       <WarningIcon style={{ margin: "4px" }} />
       <div>
-        <b>Testinstallation: </b>
-        <br /> Anfragen werden nicht ans Gesundheitsamt weitergeleitet.
+        <b>Testinstallation: </b>Anfragen werden nicht ans Gesundheitsamt
+        weitergeleitet.
       </div>
     </div>
   );
